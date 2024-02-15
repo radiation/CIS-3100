b6c54bda931b3cc845d19920617f68ab22e5b1a3
Bryan Choate <bryan.choate@gmail.com>
2024-02-15 08:30:19 -0500

    Lab2x2 and update gitignore

--- homework1/lab2x2.cpp
+++ homework1/lab2x2.cpp
@@ -1,47 +1,92 @@
+/*
+    I know I overcomplicated this, but using marginal tax rates was much more interesting than a flat tax.  I opted
+    to use a two-dimensional array to store the tax brackets and their respective marginal tax rates; I then iterated
+    through the array to calculate the total tax based on the salary.  This makes it much easier to add or remove
+    tax brackets, and the code is much more readable than a series of if-else statements.
+
+    Test data:
+
+     $20,000, 0 deps, no int =  $2,880 (12000*0.06 +  8000*0.27)
+     $20,000, 2 deps, no int =  $2,340 (12000*0.06 +  6000*0.27)
+     $40,000, 0 deps, no int =  $8,400 (12000*0.06 + 26000*0.27 + 2000*0.33)
+     $40,000, 2 deps, no int =  $7,200 (12000*0.06 + 24000*0.27)
+     $50,000, 0 deps, no int = $11,700 (12000*0.06 + 26000*0.27 + 12000*0.33)
+     $50,000, 2 deps, no int = $10,050 (12000*0.06 + 26000*0.27 +  7000*0.33)
+     $80,000, 0 deps, no int = $25,850 (12000*0.06 + 26000*0.27 + 17000*0.33 + 25000*0.50)
+     $80,000, 2 deps, no int = $21,850 (12000*0.06 + 26000*0.27 + 16000*0.33 + 25000*0.50)
+    $100,000, 0 deps, no int = $35,850 (12000*0.06 + 26000*0.27 + 17000*0.33 + 45000*0.50)
+    $100,000, 2 deps, no int = $30,850 (12000*0.06 + 26000*0.27 + 17000*0.33 + 40000*0.50)
+ */
+
 #include <iostream>
-#include <map>
 using namespace std;
 
-/*
-    I know I overcomplicated this, but using marginal tax rates was much more interesting than a flat tax.  I opted to 
-    use maps for the key/value storage, and because iterating makes it much easier to do add or change tax brackets in the future.
-    I used https://favtutor.com/blogs/iterate-through-map-cpp and https://www.programiz.com/cpp-programming/map to get the syntax correct.
- */
 int main() {
 
-    double tax_brackets[4][2] = {
+    /*
+        Two dimensional array of tax brackets.  The first element is the amount up to which the current marginal rate 
+        applies, and the second is the marginal tax rate itself.  I would have used a struct, but we haven't covered
+        those yet.
+    */
+    const double taxBrackets[][2] = {
         {0, 0.06},
         {12000, 0.27},
         {38000, 0.33},
-        {50000, 0.50}
+        {55000, 0.50}
     };
+    const int numBrackets = sizeof(taxBrackets)/sizeof(taxBrackets[0]);
 
-    double salary, tax;
+    // Variable definitions
+    double salary, totalTax = 0.0;
     int dependendents;
+    char studentLoanInterestYN;
 
-    // Prompt user for annual salary
+    // Prompt for and read user inputs
     cout << "Please enter your annual salary: ";
     cin >> salary;
 
-    // And dependents
     cout << "And how many dependents do you have? ";
     cin >> dependendents;
 
-    tax = 0;
-    int previous = 0;
+    cout << "And did you pay student loan interest? (y/n) ";
+    cin >> studentLoanInterestYN;
 
-    // Iterate through brackets and calculate total tax based on marginal tax rates
-    for (int i = 0; i < 4; i++) {
-        if (salary > tax_brackets[i][0]) {
-            tax += (tax_brackets[i][0] - tax_brackets[i-1][0]) * tax_brackets[i][1];
-            previous = tax_brackets[i][0];
-        } else {
-            tax += (salary - previous) * tax_brackets[i][1];
-            break;
-        }
+    // Reduce salary by 5% for each dependent and use this for calculations
+    double remainingSalary = salary * (1 - (0.05 * dependendents));
+
+    // If student loan interest was paid, reduce salary by that amount
+    if (studentLoanInterestYN == 'y') {
+        // No need to declare this if there was no student loan interest so we do it in the if statement
+        double studentLoanInterest;
+        cout << "How much student loan interest did you pay? ";
+        cin >> studentLoanInterest;
+        remainingSalary -= studentLoanInterest;
     }
 
-    cout << "Your total tax is " << tax << endl;
+    totalTax = 0;
+    int previous = 0;
+
+    // Iterate through all but the last bracket and calculate total tax based on marginal tax rates
+    for (int i = 0; i < numBrackets-1; i++) {
+
+        // No need to keep going
+        if (remainingSalary <= 0)
+            break;
+
+        // We could've used a ternary operator here, but I think this is more readable
+        double taxableAmount = std::min(remainingSalary , taxBrackets[i+1][0] - taxBrackets[i][0]);
+
+        // Add tax, reduce remaining taxable salary
+        totalTax += taxableAmount * taxBrackets[i][1];
+        remainingSalary -= taxableAmount;
+    }
+
+    // If there's anything left, add the final tax bracket
+    if (remainingSalary > 0) {
+        totalTax += remainingSalary * taxBrackets[numBrackets-1][1];
+    }
+
+    cout << "Your total tax is " << totalTax << endl;
 
     return 0;
 }
